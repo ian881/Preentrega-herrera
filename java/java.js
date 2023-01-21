7def40318a75b09c548d4cb28b09d36c8fe5966d
ian881 <110850006+ian881@users.noreply.github.com>
2023-01-21 16:19:03 -0300

    Update java.js

--- java/java.js
+++ java/java.js
@@ -5,8 +5,7 @@ function solicitarnombre(){
 function multiplicar (parametro1,parametro2,parametro3,parametro4){
   let resultadoM=parametro1*parametro2/parametro3 + parametro4
   alert( "pagaria con impuestos iva $"+resultadoM)
-   let total=resultadoM+costoenvio;
-   alert("Pagaria con envio $"+ total)
+   
 }
 
 solicitarnombre();
@@ -16,6 +15,26 @@ let iva=21;
 let iva2=100;
 let costoenvio=3500
 
+const paises=[
+  {
+    pais:"argentina",
+    envio:850,
+  },
+
+  {
+    pais:"chile",
+    envio:1200,
+  },
+
+  {
+    pais:"uruguay",
+    envio:1500,
+  },
+  {
+    pais:"paraguay",
+    envio:2000,
+  },
+]
 
 do {
   const monto = parseInt( prompt("Monto de su compra$$"));
@@ -27,7 +46,16 @@ multiplicar(iva,resta,iva2,resta);
   }else {
     multiplicar(iva,monto,iva2,monto) 
   }
-
  
+  let envioalpais= prompt("para que pais es el envio ?solo para uruguay,paraguay,chile y argentina")
+
+  const encontrar = paises.find( (paises)=>paises.pais === envioalpais );
+  console.log(encontrar);
+ 
+
+  alert("Usted pagaria un envio de $"+encontrar.envio
+  
+  )
+
   salir = prompt("desea usted salir");
 } while (salir != "si");
