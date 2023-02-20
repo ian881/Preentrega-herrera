fd580db2b4f985e6697b36d757882bb8dfcb361a
ian881 <ianherrera990@gmail.com>
2023-02-20 15:01:17 -0300

    NUEVO

---
+++ js/compra.js
@@ -0,0 +1,154 @@
+const compra = new Carrito();
+const listaCompra = document.querySelector("#lista-compra tbody");
+const carrito = document.getElementById('carrito');
+const procesarCompraBtn = document.getElementById('procesar-compra');
+const cliente = document.getElementById('cliente');
+const correo = document.getElementById('correo');
+
+cargarEventos();
+
+function cargarEventos() {
+    document.addEventListener('DOMContentLoaded', compra.leerLocalStorageCompra());
+
+    //Eliminar productos del carrito
+    carrito.addEventListener('click', (e) => { compra.eliminarProducto(e) });
+
+    compra.calcularTotal();
+
+    //cuando se selecciona procesar Compra
+    procesarCompraBtn.addEventListener('click', procesarCompra);
+
+    carrito.addEventListener('change', (e) => { compra.obtenerEvento(e) });
+    carrito.addEventListener('keyup', (e) => { compra.obtenerEvento(e) });
+
+}
+
+function procesarCompra() {
+    
+    if (compra.obtenerProductosLocalStorage().length === 0) {
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'No hay productos, selecciona alguno',
+            showConfirmButton: false,
+            timer: 2000
+        }).then(function () {
+            window.location = "index.html";
+        })
+    }
+    else if (cliente.value === '' || correo.value === '') {
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Ingrese todos los campos requeridos',
+            showConfirmButton: false,
+            timer: 2000
+        })
+    }
+    else {
+
+       
+
+        const textArea = document.createElement('textarea');
+        textArea.id = "detalleCompra";
+        textArea.name = "detalleCompra";
+        textArea.cols = 60;
+        textArea.rows = 10;
+        textArea.hidden = true;
+        productosLS = compra.obtenerProductosLocalStorage();
+
+        
+        textArea.innerHTML = generarTabla(productosLS).innerHTML;
+       
+        carrito.appendChild(textArea);
+
+        const btn = document.getElementById('procesar-compra');
+
+        document.getElementById('procesar-pago')
+         .addEventListener('submit', function(event) {
+           event.preventDefault();
+        
+           btn.value = 'Sending...';
+        
+           const serviceID = 'default_service';
+           const templateID = 'template_k8zjlio';
+        
+           emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+              btn.value = 'Send Email';
+              alert('Sent!');
+            }, (err) => {
+              btn.value = 'Send Email';
+              alert(JSON.stringify(err));
+            });
+        });
+
+        document.getElementById('procesar-pago')
+            .addEventListener('submit', function (event) {
+                event.preventDefault();
+
+                const cargandoGif = document.querySelector('#cargando');
+                cargandoGif.style.display = 'block';
+
+                const enviado = document.createElement('img');
+                enviado.src = 'img/mail.gif';
+                enviado.style.display = 'block';
+                enviado.width = '150';
+
+                
+
+                emailjs.sendForm(serviceID, templateID, this)
+                    .then(() => {
+                        cargandoGif.style.display = 'none';
+                        document.querySelector('#loaders').appendChild(enviado);
+
+                        setTimeout(() => {
+                            compra.vaciarLocalStorage();
+                            enviado.remove();
+                            window.location = "index.html";
+                        }, 2000);
+                    }, (err) => {
+                        cargandoGif.style.display = 'none';
+                        alert("Error al enviar el email\r\n Response:\n " + JSON.stringify(err));
+                    });
+            });
+    }
+}
+
+
+function generarTabla(productosLS) {
+    let div = document.createElement("div");
+
+    let tabla = document.createElement("table");
+    
+    tabla.innerHTML += `<table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Precio</th>
+                                <th scope="col">Cantidad</th>
+                                <th scope="col">Sub total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>`;
+
+    const body = tabla.childNodes[3];
+
+    
+    productosLS.forEach(producto => {
+        const row = document.createElement("tr");
+        row.innerHTML += `
+                            <td>${producto.titulo}</td>
+                            <td>${producto.precio}</td>
+                            <td>${producto.cantidad}</td>
+                            <td>${producto.precio * producto.cantidad}</td>
+                        `;
+        body.appendChild(row);
+    });
+
+    tabla.appendChild(body);
+    div.appendChild(tabla);
+    return div;
+}
